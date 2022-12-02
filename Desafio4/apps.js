a9d37e3c97adac3dab0dbd3afed28f21403cefc0
CurthbertAllgood <101150271+CurthbertAllgood@users.noreply.github.com>
2022-12-01 21:17:20 -0300

    actualizacion desafio 4

--- Desafio4/apps.js
+++ Desafio4/apps.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const productosRouter = require('./routes/productos')
-
+const morgan = require('morgan');
 const app=express()
 
 const server = app.listen(8080,()=> console.log('server up'))
 
 
+app.use(morgan('dev'))
 app.use(express.json())
-app.use(express.static('public'))
+
+app.use('/', express.static('public'))
+app.use(express.static(__dirname + '/public'));
 app.use('/api/productos', productosRouter )
